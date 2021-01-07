dc1a3be6305e05c7b5be3db3941b6c71993943fe
Mike_Zhang1 <751603405@qq.com>
2021-01-07 19:22:04 +0800

    'sdsd'

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -2,7 +2,7 @@
   <div class="home-page">
     <div class="banner">
       <div class="container">
-        <h1 class="logo-font">你是谁啊，怎么老是出问题</h1>
+        <h1 class="logo-font">你是谁啊，dfdfdfdf怎么老是出问题</h1>
         <p>A place to share your knowledge.</p>
       </div>
     </div>
